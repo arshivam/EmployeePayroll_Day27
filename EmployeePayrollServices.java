d02adb8ce1d0b5ee466a363e6ec1bc25b0c3eb6d
arshivam <shivam.armarkar007@gmail.com>
2022-03-14 15:11:40 +0530

    Added programme to read and write the data from console

--- EmployeePayrollServices.java
+++ EmployeePayrollServices.java
@@ -1,5 +1,31 @@
 package com.bridgelabz.day27;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EmployeePayrollServices {
+
+	ArrayList<EmployeePayrollData> employeePayrolldata = new ArrayList<>();
+
+	public static void main(String[] args) {
+		EmployeePayrollServices service = new EmployeePayrollServices();
+		Scanner sc = new Scanner(System.in);
+		service.readEmployeePayrollData(sc);
+		service.writeEmployeePayrollData();
+	}
+
+	public void readEmployeePayrollData(Scanner sc) {
+		System.out.println("Enter Employee ID: ");
+		int id = sc.nextInt();
+		System.out.println("Enter Employee Name: ");
+		String name = sc.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = sc.nextDouble();
+		employeePayrolldata.add(new EmployeePayrollData(id, name, salary));
+	}
+
+	public void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrolldata);
+	}
 
 }
